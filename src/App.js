ec911e076375747c98e1767f3f633547bd480179
Temirbaev Temirlan <chinese-phones-premium@mail.ru>
2022-04-09 23:28:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, {useState, useEffect} from 'react';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './Components/NewsCards/NewsCards';
+const alanKey = 'b5e668854e71ecbf7ed107fea99a9dfe2e956eca572e1d8b807a3e2338fdd0dc/stage';
+const App = () => {
+    const [newsArticles, setNewsArticles] = useState([])
+    const [activeArticle, setActiveArticle] = useState(-1)
+    useEffect(() => {
+        alanBtn({
+            key : alanKey,
+            onCommand : ({command, articles, number}) => {
+                if(command === 'newHeadlines'){
+                    setNewsArticles(articles);
+                } else if(command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if(command === 'open'){
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy : true}) : number
+                    const article = articles[parsedNumber - 1]
+                    if (article){
+                        window.open(article.url, '_blank')
+                    }
+                }
+            }
+        })
+    }, [])
+    
+  return <div>
+        <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+  </div>
+};
 
 export default App;
